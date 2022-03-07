ef4c02f79bdb7b198c9b6bdf230425f8bab0cbc6
Zhu Boao <361255448@qq.com>
2022-03-07 21:24:58 +0800

    First Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// nuxt.config.ts
+import { defineNuxtConfig } from 'nuxt3'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  css: ["@/assets/css/styles.css"],
+  build: {
+    postcss: {
+      postcssOptions: require("./postcss.config.js"),
+    },
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<!-- pages/index.vue -->
+<script setup>
+definePageMeta({
+  layout: "blog",
+});
+const { data: posts } = await useFetch(
+  "https://jsonplaceholder.typicode.com/posts"
+);
+</script>
+
+<template>
+  <div>
+    <article class="m-4 md:w-1/2 lg:w-1/3" v-for="post in posts" :key="post.id">
+      <NuxtLink :to="`/post-${post.id}`">
+        <h2 class="mb-2 capitalize text-2xl font-semibold">
+          {{ post.title }}
+        </h2>
+      </NuxtLink>
+      <p>{{ post.body }}...</p>
+    </article>
+  </div>
+</template>
